09f894aeac7d8575e70a51b451169d712048f87c
olawalemayor <olawalemayor90@gmail.com>
2022-06-28 23:20:53 +0100

    added env variables

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import config from "config";
 
-const dbURL: string = config.get("dbHost");
-const dbName: string = config.get("dbName");
-const dbUser: string = config.get("dbUser");
-const dbPass: string = config.get("dbPass");
+const dbURL: string = process.env.DB_HOST || config.get("dbHost");
+const dbName: string = process.env.DB_NAME || config.get("dbName");
+const dbUser: string = process.env.DB_USER || config.get("dbUser");
+const dbPass: string = process.env.DB_PASSWORD || config.get("dbPass");
 
 const conn = mongoose.connect("mongodb://" + dbURL, {
   dbName,

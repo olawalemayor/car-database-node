ffbd2744142584e7f7ce434fd26c246e2e881a81
olawalemayor <olawalemayor90@gmail.com>
2022-06-30 14:07:09 +0100

    Adjusted model and schema to reflect engine in response

--- server/src/repositories/cars/carSchema.ts
+++ server/src/repositories/cars/carSchema.ts
@@ -1,48 +1,11 @@
 import mongoose from "mongoose";
-import Car, { Engine } from "./carModel";
-
-export const engineSchema = new mongoose.Schema<Engine>({
-  Cylinders: String,
-  Displacement: String,
-  Power: String,
-  Torque: String,
-  "Fuel System": String,
-  Fuel: String,
-  "Fuel capacity": String,
-  "Top Speed": String,
-  "Acceleration 0-62 Mph (0-100 kph)": String,
-  "Drive Type": String,
-  Gearbox: String,
-  Front: String,
-  Rear: String,
-  "Tire Size": String,
-  Length: String,
-  Width: String,
-  Height: String,
-  "Front/rear Track": String,
-  Wheelbase: String,
-  "Ground Clearance": String,
-  "Cargo Volume": String,
-  "Turning circle (curb to curb)": String,
-  "Aerodynamics (Cd)": String,
-  "Unladen Weight": String,
-  "Gross Weight Limit": String,
-  City: String,
-  Highway: String,
-  Combined: String,
-  "CO2 Emissions": String,
-});
+import Car from "./carModel";
 
 export const CarSchema = new mongoose.Schema<Car>({
   url: String,
   model: String,
   images_links: [String],
-  engines: [
-    {
-      type: Map,
-      of: engineSchema,
-    },
-  ],
+  engines: [],
   description: String,
   brand: String,
   brochures: [String],

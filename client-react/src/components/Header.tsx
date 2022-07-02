61709cfc573b1d2fcff259bb717cf3b5f378a1dc
olawalemayor <olawalemayor90@gmail.com>
2022-07-02 23:49:15 +0100

    Adjusted the landing page and header

--- client-react/src/components/Header.tsx
+++ client-react/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FilterService from "../services/FilterService";
 import { ResponseResult } from "../models";
@@ -10,6 +10,15 @@ interface HeaderProp {
 
 export default function Header({ setFilterCars }: HeaderProp) {
   const navigate = useNavigate();
+  const [isSrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    if (window.screen.width <= 768) {
+      window.addEventListener("scroll", () =>
+        window.scrollY === 0 ? setIsScrolled(false) : setIsScrolled(true)
+      );
+    }
+  }, []);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -33,12 +42,19 @@ export default function Header({ setFilterCars }: HeaderProp) {
     navigate("");
   };
   return (
-    <div className="flex flex-col lg:flex-row items-center m-2 lg:m-4 lg:justify-between">
-      <h1 className="my-2 lg:w-[30%] font-bold text-2xl text-center italic text-blue-700">
-        <NavLink to="">CAR DATABASE</NavLink>
-      </h1>
+    <div className="flex flex-col lg:flex-row items-center m-2 lg:m-4 lg:justify-between py-2 lg:py-0">
+      {!isSrolled && (
+        <h1 className="my-2 lg:w-[30%] font-bold text-2xl text-center italic text-blue-700">
+          <NavLink to="">
+            CAR{" "}
+            <span className="text-black not-italic border-2 p-2 rounded-md">
+              DATABASE
+            </span>
+          </NavLink>
+        </h1>
+      )}
 
-      <div className="focus-within:w-full lg:focus-within:w-1/2 border-2 border-blue-700 rounded-l-full rounded-r-full p-2 lg:pl-2">
+      <div className="focus-within:w-full lg:focus-within:w-1/2 border-2 border-blue-700 rounded-l-full rounded-r-full p-2 lg:pl-2 mt-5 lg:mt-0">
         <form onSubmit={handleSubmit}>
           <input
             type="text"
--- client-react/src/components/result/SearchResult.tsx
+++ client-react/src/components/result/SearchResult.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import filterContext from "../../context/FilterContext";
 import { useNavigate } from "react-router-dom";
+import placeholderImage from "../../assets/images/pexels-khalid-satvilker-10590576.jpg";
 
 export default function SearchResult() {
   const [filterCars] = useContext(filterContext);
@@ -8,33 +9,53 @@ export default function SearchResult() {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-white w-full h-full p-2">
-      {filterCars.result &&
-        filterCars.result.map(
-          ({ brand, model, images_links, description, _id }, index) => (
-            <div
-              className="flex flex-col lg:flex-row p-2 bg-slate-100 hover:bg-slate-300 cursor-pointer border border-blue-500 rounded-md mb-2"
-              key={index}
-              onClick={() => navigate(`car/${_id}`)}
-            >
-              <div className="w-full lg:w-[40%] xl:w-[30%] mt-2 px-2">
-                <div className="w-full lg:w-[200px] h-[250px] lg:h-[200px] bg-black">
-                  <img
-                    src={images_links[0]}
-                    alt={`${model} ${brand}`}
-                    className="w-full h-full"
-                  />
-                </div>
-              </div>
-
-              <div className="w-full lg:w-[60%] xl:w-[70%]">
-                <div className="h-48 overflow-hidden text-black">
-                  {description}
-                </div>
-              </div>
-            </div>
-          )
+    <>
+      <div className="border-b-4 border-red-600 py-2 flex items-end">
+        <h2 className="text-white text-2xl lg:text-5xl">SEARCH RESULT:</h2>
+        {!filterCars.result.length && (
+          <div className="text-red-500 ml-2 text-sm md:text-base lg:text-lg">
+            Search for your vehicle
+          </div>
         )}
-    </div>
+      </div>
+
+      <div className="bg-white w-full h-full p-2">
+        {!filterCars.result.length && (
+          <img
+            src={placeholderImage}
+            alt="by Khalid Satvilker"
+            className="w-full h-auto opacity-70"
+            onContextMenu={(e) => e.preventDefault()}
+          />
+        )}
+
+        {filterCars.result &&
+          filterCars.result.map(
+            ({ brand, model, images_links, description, _id }, index) => (
+              <div
+                className="flex flex-col lg:flex-row p-2 bg-slate-100 hover:bg-slate-300 cursor-pointer border border-blue-500 rounded-md mb-2"
+                key={index}
+                onClick={() => navigate(`car/${_id}`)}
+              >
+                <div className="w-full lg:w-[40%] xl:w-[30%] mt-2 px-2">
+                  <div className="w-full lg:w-[200px] h-[250px] lg:h-[200px] bg-black">
+                    <img
+                      src={images_links[0]}
+                      alt={`${model} ${brand}`}
+                      className="w-full h-full"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full lg:w-[60%] xl:w-[70%]">
+                  <div className="h-48 overflow-hidden text-black">
+                    {description}
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+      </div>
+    </>
   );
 }
